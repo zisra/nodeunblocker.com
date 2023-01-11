1d99793088472916776b527b0909d691823b1876
zisra <100528712+zisra@users.noreply.github.com>
2023-01-11 00:49:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,53 +14,11 @@ var querystring = require('querystring');
 var express = require('express');
 var Unblocker = require('unblocker');
 var Transform = require('stream').Transform;
-var youtube = require('unblocker/examples/youtube/youtube.js')
 
 var app = express();
 
-var google_analytics_id = process.env.GA_ID || null;
-
-function addGa(html) {
-    if (google_analytics_id) {
-        var ga = [
-            "<script type=\"text/javascript\">",
-            "var _gaq = []; // overwrite the existing one, if any",
-            "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
-            "_gaq.push(['_trackPageview']);",
-            "(function() {",
-            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
-            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
-            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
-            "})();",
-            "</script>"
-            ].join("\n");
-        html = html.replace("</body>", ga + "\n\n</body>");
-    }
-    return html;
-}
-
-function googleAnalyticsMiddleware(data) {
-    if (data.contentType == 'text/html') {
-
-        // https://nodejs.org/api/stream.html#stream_transform
-        data.stream = data.stream.pipe(new Transform({
-            decodeStrings: false,
-            transform: function(chunk, encoding, next) {
-                this.push(addGa(chunk.toString()));
-                next();
-            }
-        }));
-    }
-}
-
 var unblocker = new Unblocker({
-    prefix: '/proxy/',
-    requestMiddleware: [
-        youtube.processRequest
-    ],
-    responseMiddleware: [
-        googleAnalyticsMiddleware
-    ]
+    prefix: '/proxy/'
 });
 
 // this line must appear before any express.static calls (or anything else that sends responses)
